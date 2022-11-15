2b409fdcd9b3f389438ba87fbdf9660746feb970
Willian Thiago <50601644+Willian-ThiagoFZ@users.noreply.github.com>
2022-11-14 22:55:26 -0300

    Functions have been refactored.

--- refactor.js
+++ refactor.js
@@ -1,27 +1,22 @@
-function returnRandomNumber() {
-  return Math.random();
+function calculateNumberByRandom(number, operator) {
+  // Instacia um Número Aleatório
+  const numberRandon = Math.round(Math.random() * 100);
+
+  // Objeto que evidencia o resultado para cada tipo de operador matemático
+  const mathOperators = {
+    sum: `Seu número é ${numberRandon + number}!`,
+    subtract: `Seu número é ${numberRandon - number}!`,
+    divide: `Seu número é ${numberRandon / number}!`,
+    multiply: `Seu número é ${numberRandon * number}!`,
+  };
+
+  // verifica se o número enviado é do tipo "Number" e se o operador Lógico é válido (Evitar Bugs !!)
+  if (typeof number === "number" && Object.keys(mathOperators).includes(operator)){
+    return mathOperators[operator];
+  }else{
+    return "Digite um Número e um Operador Matemático Válido."
+  }
 }
 
-function sumToRandomNumber(num) {
-  const numberToSum = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToSum * 100) + num}!`;
-}
-
-function subtractRandomNumber(num) {
-  const numberToSubtract = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToSubtract * 100) - num}!`;
-}
-
-function multiplyToRandomNumber(num) {
-  const numberToMultiply = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToMultiply * 100) * num}!`;
-}
-
-function divideRandomNumber(num) {
-  const numberToDivide = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToDivide * 100) / num}!`;
-}
+// Executa a função passando paràmetros válido (number, operador)
+console.log(calculateNumberByRandom(25, "multiply"));
